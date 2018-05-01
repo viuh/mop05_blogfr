8dc4016750a4f9b1ca96c7189a67339dc911ba18
viuh <37941072+viuh@users.noreply.github.com>
2018-05-01 11:04:26 +0300

    -5.6 nearly...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import Blog from './components/Blog'
+
 import blogService from './services/blogs'
+import userService from './services/users'
 import Notification from './components/Notification'
 //import noteService from './services/notes'
 import loginService from './services/login'
 import './index.css'
 import Togglable from './components/Togglable'
+import TogglableDiv from './components/TogglableDiv'
+
 import LoginForm from './components/LoginForm'
+const jwt = require('jsonwebtoken')
+
+
+const Blogy = ({blog, fu1}) => {
+  return
+  (
+  <div className="blogbody">
+  <a target="_new" href="`blog.url`">{blog.url}</a>
+  <form onSubmit={fu1}>
+  {blog.likes} likes <button>like</button><br/>
+  </form>
+  added by !!!{blog.user} .!!!
+  </div>
+  )
+}
+
+const showBlogsit = ({allblogs, blog, fu1, visibleOne}) => {
+  
+  return (
+  <div>    
+    <h2>Blogs</h2>
+    {allblogs.map(blog => 
+    <div  key={blog._id} id={blog._id} >
+      <Blog key={blog._id} id={blog._id} blog={blog} />
+      </div>
+      )
+    }
+    </div>
+  )
+}
+
+
 
 
 class App extends React.Component {
@@ -20,33 +56,51 @@ class App extends React.Component {
       user: null,
       currentuser: null,
       title: '',
-      author:'',
+      author:'',  
       url:'',
+      blogvisible: false,
+      blogvisibleid: '', 
+      lastopened: null,
+      clicksdone : 0
     }
   }
 
   addBlog = async (event) => {
     event.preventDefault()
+    let tokeni = this.state.user.token
+
+    let secru = "aksdjuioouimzxcvsdfd"
 
     try {
 
+      //console.log('proces.secr', secru)
+      const decodedToken = jwt.verify(tokeni, secru)
 
-
+      console.log('Blogin lisaaja: ', decodedToken.id)
+      let bname = this.state.title
+  
+      //console.log('Blogin luojanen:', createdBlog._id,':', this.state.currentuserid, 'Tai:', this.state.user)
+ 
+      //let allusers = await userService.getAll()
+      //let theOne = allusers.find({_id: decodedToken.id})
+      //console.log('*** Usei', theOne)
+      
       const miBlog = {
         title: this.state.title === null ? null : this.state.title,
         author: this.state.author,
-        url: this.state.url        
+        url: this.state.url,               
+        user: decodedToken.id
       }
-  
-      let bname = this.state.title
   
       const createdBlog = await blogService.create(miBlog)
 
       console.log('Luotiin?', createdBlog)
+      //console.log('user data: ', allusers)
+      let existingBlogs = this.state.blogs.concat(createdBlog)
 
       this.setState({
         title: '', author: '', url:''
-      , blogs : this.state.blogs.concat(createdBlog),
+      , blogs : existingBlogs,
         error: `Blog '${bname}' added.`,
         msgtype: 'info' 
       })
@@ -63,6 +117,7 @@ class App extends React.Component {
       setTimeout(() => {
         this.setState({ error: null , msgtype:null })
       }, 5000)
+      console.log('No blog added:', exception)
     }
   }
 
@@ -80,10 +135,11 @@ class App extends React.Component {
       
       blogService.setToken(user.token)
 
-      console.log('Useri:', user)
+      //console.log('Useri:', user)
 
+      this.setState({ currentuserid : user._id})
       this.setState({ currentuser : user.name})
-      this.setState({ username: '', password: '', user })
+      this.setState({ username: '', password: '', user: user })
     } catch (exception) {
       this.setState({
         msgtype: 'error',
@@ -107,15 +163,36 @@ class App extends React.Component {
     this.setState({ showAll: !this.state.showAll })
   }
 
+  handleClick = (idx) => {
+    //https://stackoverflow.com/a/47130799/364931
+    //console.log("AAA",text,' id ',idx, ' eventti : ', event);
+    //console.log('AAA', idx.id)
 
-  componentDidMount() {
-    blogService.getAll().then(blogs =>
-      this.setState({ blogs })
-    )
+    let temp = idx.id
+    let kliksui = this.state.clicksdone + 1
+
+    this.setState({ blogvisibleid : idx.id,
+      clicksdone: kliksui ,
+      lastopened : temp
+    })
+  }
+
+
+  async componentDidMount ()  {
+    
+//    blogService.getAll().then(blogs =>
+//      this.setState({ blogs })
+//    )
+    let kama = await blogService.getAll()
+    this.setState({blogs: kama})
+
+   // const allBlogs = await blogService.getAll()
+    //this.setState({allBlogs})
+
     const loggedUserJSON = window.localStorage.getItem('loggedappUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
-      this.setState({user})
+      this.setState({user: user})   // user?
       blogService.setToken(user.token)
     }
   } 
@@ -123,7 +200,6 @@ class App extends React.Component {
   logout = async (event) => {
     window.localStorage.clear() // kaek pois!
   }
-
 
   render() {
 
@@ -133,7 +209,57 @@ class App extends React.Component {
       <div>
         <h2>Blogs</h2>
         {this.state.blogs.map(blog => 
-          <Blog key={blog._id} id={blog._id} blog={blog}/>
+          <Blog key={blog._id} id={blog._id} blog={blog}
+          adder='tbs'
+          fu1={()=>this.handleClick(blog)}
+          visible={this.state.blogvisibleid}
+          lastopened = {this.state.lastopened}
+          counter = {this.state.clicksdone}
+          />
+        )}
+      </div>
+    )
+
+
+    const showBlogs3 = () => (
+      <div>    
+        <h2>Blogs</h2>
+        {this.state.blogs.map(blog => 
+        
+        <TogglableDiv key={blog._id} id={blog._id} ref={component => this.blogRow = component}>
+          <Blog key={blog._id} id={blog._id} blog={blog} />
+          </TogglableDiv>
+          )
+        }
+        </div>
+    )
+
+    const showBlogs2 = () => (
+      <div>    
+        <h2>Blogs</h2>
+        {this.state.blogs.map(blog => 
+        
+          <Blog key={blog._id} id={blog._id} blog={blog} 
+          adder={blog.user===null ? 'none' : blog.user }
+          fu1={(e)=>this.handleClick(e,blog._id)} 
+          classStyle={this.state.blogvisible ? 'blogbody, visible':'blogbody, hidden'}
+          
+          />
+          )
+        }
+        </div>
+    )
+
+
+    const showBlogs1 = () => (
+
+      <div>
+        <h2>Blogs</h2>
+        {this.state.blogs.map(blog => 
+          <Blog key={blog._id} id={blog._id} blog={blog}
+          adder='tbd'
+          fu1={(e)=>this.handleClick(e,blog._id)}
+          />
         )}
       </div>
     )
@@ -215,6 +341,7 @@ class App extends React.Component {
     return (
       <div>
       <Notification message={this.state.error} msgtype={this.state.msgtype} />
+
 
       <h2>Welcome to blog app</h2>
       {this.state.user === null ?
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,50 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div id={blog.id}>
-    {blog.title} {blog.author}
+import '../index.css'
+
+//  {  console.log('Blogi:',blog.title, '-url-:', blog.url) }
+//let hideWhenVisible = { display: visible === blog.id ? 'none' : '' }
+//let showWhenVisible = { display: visible === blog.id ? '' : 'none' }
+
+const tyyli = ({blog,visible,lastopened,clicksdone}) => {
+
+  //console.log('Toggling ', blog.id)
+  let res
+  /*let res = visible === blog.id ? 
+  { 'display': visible === blog.id ? 'none' : '' } 
+: { 'display': visible === blog.id ? '' : 'none' } 
+*/
+
+  //var isVisible = _.('#blog.id').is(':visible')
+
+  if ( visible === blog.id) {
+    //console.log('Toggling', blog.id, ' vs ', visible)
+      res = 'visible'
+    } else {
+      res =  'hidden'  
+    }
+
+    if (visible === blog.id === lastopened) {
+      res = 'hidden'
+    }
+
+  return res
+}
+
+
+const Blog = ({blog, adder, fu1, fu2, classStyle, visible, lastopened,clicksdone}) => (
+
+  <div>
+    <div className="blogheader" onClick={fu1}>{blog.title} {blog.author}</div>
+    <div key={blog._id} id="{blog._id}" classnxame="{classStyle}"
+    className={tyyli({blog,visible, lastopened,clicksdone})} >
+<a target="_new" href="`blog.url`">{blog.url}</a>
+<form onSubmit={fu2}>
+{blog.likes} likes <button>like</button><br/>
+</form>
+added by {blog.user !== null ? blog.user.name : '-'   }
+</div>
   </div>  
 )
 
 export default Blog
+
