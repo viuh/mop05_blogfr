6cbea99ced9c944f489c16fccd5fe4b05492fa4c
viuh <37941072+viuh@users.noreply.github.com>
2018-05-10 20:29:04 +0300

    5.14 :coffee

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,12 +9,6 @@ const tyyli = ({blog,visible,lastopened,clicksdone}) => {
 
   //console.log('Toggling ', blog.id)
   let res
-  /*let res = visible === blog.id ? 
-  { 'display': visible === blog.id ? 'none' : '' } 
-: { 'display': visible === blog.id ? '' : 'none' } 
-*/
-
-  //var isVisible = _.('#blog.id').is(':visible')
 
   if ( visible === blog.id) {
     //console.log('Toggling', blog.id, ' vs ', visible)
@@ -36,12 +30,12 @@ const Blog = ({blog, adder, fu1, fu2, classStyle, visible, lastopened,clicksdone
 
     <div key={blog._id}>
     <div className="blogheader" onClick={fu1}>{blog.title} {blog.author}</div>
-    <div key={blog._id} id="{blog._id}" classnxame="{classStyle}"
+    <div key={blog._id} id="{blog._id}" name="blogcontent" classnxame="{classStyle}"
     className={tyyli({blog,visible, lastopened,clicksdone})} >
 <a target="_new" href="`blog.url`">{blog.url}</a>
-&nbsp;&nbsp;{blog.likes} likes <button onClick={fu2}>like</button><br/>
+&nbsp;&nbsp;{blog.likes} likes <button className="likebutton" onClick={fu2}>like</button><br/>
 { (showdelete === true) ? 
-  (<div></div>) : (<button onClick={fu3}>Delete</button> ) }
+  (<div></div>) : (<button className="deletebutton" onClick={fu3}>Delete</button> ) }
 </div>
    </div>
 )
