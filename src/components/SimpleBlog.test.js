2baa53962c0a502521cf89ef3a1114f5a4b74e6c
viuh <37941072+viuh@users.noreply.github.com>
2018-05-10 19:01:26 +0300

    5.13 :coffee:

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -32,5 +32,34 @@ describe.only('<SimpleBlog />', () => {
     expect(bodyDiv.text()).toContain(blog.likes)
 
 
+
   })
+
+
+  it('clicking the like button twice calls event handler twice', () => {
+    const blog = {
+      title: "testaanpa testailen 2",
+      author: "N. Nobody",
+      likes: 4
+    }
+  
+    const mockHandler = jest.fn()
+  
+    const blogComponent = shallow(
+      <SimpleBlog 
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+  
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
+  
+
 })
+
+
+
